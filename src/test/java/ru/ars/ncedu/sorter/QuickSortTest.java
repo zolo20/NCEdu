42e1204360e2d4d9556544b09bc8db8f0198b2ef
zolo20 <oraljibek@gmail.com>
2018-06-01 00:25:58 +0300

    VHS

---
+++ src/test/java/ru/ars/ncedu/sorter/QuickSortTest.java
@@ -0,0 +1,35 @@
+package ru.ars.ncedu.sorter;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class QuickSortTest {
+
+    @Test
+    public void sortTest() {
+        Integer[] array = GenerationArrayInteger.arrayGeneration(new Random().nextInt(10));
+        System.out.println(Arrays.toString(array));
+        QuickSort.sort(array);
+        if (array.length > 0) {
+            Assert.assertTrue(ArraysSortChecker.isSort(array));
+        } else {
+            Assert.assertFalse(ArraysSortChecker.isSort(array));
+        }
+    }
+
+    @Test
+    public void sortComparatorTest() {
+        Integer[] array = GenerationArrayInteger.arrayGeneration(new Random().nextInt(10));
+        System.out.println(Arrays.toString(array));
+        QuickSort.sort(array, (o1, o2) -> Integer.compare(o2.compareTo(o1), 0));
+        if (array.length > 0) {
+            Assert.assertTrue(ArraysSortChecker.isSort(array));
+        } else {
+            Assert.assertFalse(ArraysSortChecker.isSort(array));
+        }
+    }
+}
