df8b97bceef67d6638672d61606cfa0e8f2f1e6c
zolo20 <oraljibek@gmail.com>
2018-06-26 15:07:59 +0300

    VHS

--- src/main/java/ru/ars/ncedu/task5/pingpong/PingPongMain.java
+++ src/main/java/ru/ars/ncedu/task5/pingpong/PingPongMain.java
@@ -1,30 +1,40 @@
 package ru.ars.ncedu.task5.pingpong;
 
-
 public class PingPongMain {
-    private static  String state = "Pong";
+    private static final Object monitor = new Object();
+    private static String state = "Pong";
 
-    public static void main(String[] args) throws InterruptedException {
-        for (;;) {
+    public static void main(String[] args) {
+        for (int i = 0;i < 10;i++) {
             Thread ping = new Thread(() -> {
-                if (state.equals("Pong")) {
-                    state = "Ping";
-                    System.out.println(1+state);
+                synchronized (monitor) {
+                    try {
+                        while (!state.equals("Pong")) monitor.wait();
+                        state = "Ping";
+                        System.out.println(state);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    monitor.notify();
+
                 }
             });
 
             Thread pong = new Thread(() -> {
-               if (!ping.isAlive()) {
-                   if (state.equals("Ping")) {
+               synchronized (monitor) {
+                   try {
+                       while (!state.equals("Ping")) monitor.wait();
                        state = "Pong";
-                       System.out.println(2 + state);
-                       System.out.println("-------");
+                       System.out.println(state);
+                       System.out.println("--------");
+                   } catch (InterruptedException e) {
+                       e.printStackTrace();
                    }
+                   monitor.notify();
                }
             });
             ping.start();
             pong.start();
-            pong.join();
         }
     }
 }
