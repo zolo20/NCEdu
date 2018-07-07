b84af8a39f6aabb275a790002ab7f5898d58dc1c
zolo20 <oraljibek@gmail.com>
2018-07-07 03:16:30 +0300

    VHS

--- src/main/java/ru/ars/ncedu/pattern/FactoryMethod/MainFactoryMethod.java
+++ src/main/java/ru/ars/ncedu/pattern/FactoryMethod/MainFactoryMethod.java
@@ -2,6 +2,11 @@ package ru.ars.ncedu.pattern.FactoryMethod;
 
 class MainFactoryMethod {
     public static void main(String[] args) {
+        /*
+        RedPen redPen = new RedPen();
+        redPen.write();
+        */
+
         CreatorPen creator = new CreatorBluePen();
         Pen pen = creator.createPen();
         pen.write();
---
+++ src/main/java/ru/ars/ncedu/task6_5/WeightLess.java
@@ -0,0 +1,61 @@
+package ru.ars.ncedu.task6_5;
+
+import java.util.Arrays;
+
+public class WeightLess {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 5, 1, 8},
+                {1, 2, 3, 7},
+                {1, 10, 2, 5},
+                {1, 1, 1, 1}
+        };
+        int[][] marker = new int[matrix.length][matrix[0].length];
+        for (int k = 0; k < marker.length; k++) {
+            for (int i = 0; i < marker[k].length; i++) {
+                marker[k][i] = -1;
+            }
+        }
+        marker[0][0] = matrix[0][0];
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if (row != matrix.length - 1 && col != matrix[row].length - 1) {
+                    if (marker[row][col + 1] == -1) {
+                        marker[row][col + 1] = marker[row][col] + matrix[row][col + 1];
+                    } else {
+                        int val = marker[row][col] + matrix[row][col + 1];
+                        if (val < marker[row][col + 1]) {
+                            marker[row][col + 1] = val;
+                        }
+                    }
+
+                    if (marker[row + 1][col] == -1) {
+                        marker[row + 1][col] = marker[row][col] + matrix[row + 1][col];
+                    } else {
+                        int val = marker[row][col] + matrix[row + 1][col];
+                        if (val < marker[row + 1][col]) {
+                            marker[row + 1][col] = val;
+                        }
+                    }
+                } else {
+                    if (col != matrix[row].length - 1) {
+                        if (marker[row][col + 1] == -1) {
+                            marker[row][col + 1] = marker[row][col] + matrix[row][col + 1];
+                        } else {
+                            int val = marker[row][col] + matrix[row][col + 1];
+                            if (val < marker[row][col + 1]) {
+                                marker[row][col + 1] = val;
+                            }
+                        }
+                    }
+
+                    if (row != matrix.length - 1) {
+                        marker[row + 1][col] = marker[row][col] + matrix[row + 1][col];
+                    }
+                }
+            }
+        }
+        System.out.println(marker[marker.length - 1][marker[marker.length - 1].length - 1]);
+    }
+}
